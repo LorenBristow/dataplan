fb657f5f186c4c9de1548cb22f37f3354a5bc086
Loren Bristow <lorendup@gmail.com>
2018-11-22 10:39:51 +0000

    chatterbox page html done with js connected

---
+++ js/calc-lonewolf.js
@@ -0,0 +1,37 @@
+/* Variables we need for the game */
+
+var wolf_gb = '';
+var wolf_price = 0;
+var frequency = '';
+var total = 0;
+//var final_price = 0;
+
+/* Listen for the button clicks*/
+$('.wolf--price').click(function(event) {
+  wolf_gb = this.getAttribute("data-gb");
+  wolf_price = parseInt(this.getAttribute("data-price"));
+  console.log('price ' + wolf_price);
+  //$('#wolf_price').text(wolf_price);
+});
+
+$('.frequency').click(function(event) {
+  frequency = this.getAttribute("data-freq");
+  $('#frequency').text(frequency);
+  console.log('freq1 ' + frequency);
+  calculator();
+  // wolf_price = 0;
+  console.log('freq2 ' + wolf_price);
+
+});
+
+function calculator() {
+
+  if (frequency == "per annum") {
+    total = 12 * wolf_price;
+  } else if (frequency == "per month") {
+    total = wolf_price;
+  }
+  $('#wolf_price').text(total);
+
+  console.log('calc ' + total);
+}
